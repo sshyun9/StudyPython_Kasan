7aef3cf538af6f6e15220c710631face12015342
sshyun9 <seunghyun.park9587@gmail.com>
2022-05-23 17:11:47 +0900

    220523 깃헙 최초 커밋

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# StudyPython_Kasan
+# StudyPython
 파이썬 기초 학습 리포지토리
+
+## 1일차
+- 깃, 깃헙 설정
+- 파이썬, Visual Studio Code 설치
+- 파이썬 기본 학습
+    - 변수
+    - 자료형
+    - 연산자
---
+++ day1/test.py
@@ -0,0 +1,22 @@
+from cmath import pi
+
+
+print('Hello, Python!')
+
+a = 'Hello, Python.'
+print(a)
+a = False
+print(a)
+a = 10
+print(a)
+a=pi
+print(a)
+
+#변수명 지정
+var1= 17
+print(var1)
+Var1 = 19
+print(Var1)
+print(var1)
+
+print(var1==Var1)
